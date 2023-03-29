f362f0b1361eb96e496f9d718e3bc7a72768269c
Stanislav Tonkich <st.tonkikh@gmail.com>
2023-03-29 21:42:44 +0200

    add 404 Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const NotFoundPage = () => {
   return (
     <div>
-      <img src={require("../images/notfound-.jpg")} alt="404 Not Found" />
+      <img src={require("../images/notfound.jpg")} alt="404 Not Found" />
     </div>
   );
 };
